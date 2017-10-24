998b8efcb3d22ae80379739dfff6b02ef899e3f3
ztstroud <ztstroud@gmail.com>
2017-10-24 14:05:44 -0600

    Finalized render logic

--- vectorGraphics.js
+++ vectorGraphics.js
@@ -1,87 +1,95 @@
 var canvas = document.getElementById("canvas");
 var ctx    = canvas.getContext("2d");
 
-var entity = Entity(50, 50, 0, 1, 0, 0, [Point(25, 25), Point(25, -25), Point(-25, -25), Point(-25, 25)]);
+var square = new Entity(canvas.width / 3, canvas.height / 2, 0, 1, 0, 0, [new Point(25, 25), new Point(25, -25), new Point(-25, -25), new Point(-25, 25)], false);
+
+var diamond = new Entity(2 * canvas.width / 3, canvas.height / 2, 0, 1, 0, 0, [new Point(25, 0), new Point(0, 25), new Point(-25, 0), new Point(0, -25)]);
 
 redraw();
 setInterval(redraw, 32);
 
 function redraw() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
-    entity.draw(ctx);
+    
+    square.draw(ctx);
+    diamond.draw(ctx);
 }
 
-function Entity(x, y, angle, scale, xShear, yShear, points) {
-    entity = {};
+function Entity(x, y, angle, scale, xShear, yShear, points, closeShape = true) {
+    this.x = x;
+    this.y = y;
     
-    entity.x = x;
-    entity.y = y;
+    this.angle = angle;
+    this.scale = scale;
     
-    entity.angle  = angle;
-    entity.scale  = scale;
-    entity.xShear = xShear;
-    entity.yShear = yShear;
+    this.xShear = xShear;
+    this.yShear = yShear;
     
-    entity.points = points;
-    entity.draw   = drawEntity;
+    this.points     = points;
+    this.closeShape = closeShape;
     
-    return entity;
+    this.draw = function() {
+        var matrix = [[Math.cos(this.angle) * this.scale, (-Math.sin(this.angle) + this.xShear) * this.scale],
+                 [(Math.sin(this.angle) + this.yShear) * this.scale, Math.cos(this.angle) * this.scale]];
+    
+        // calculate the onscreen position of all the points in the entity
+        var points = [];
+        for(pointIndex in this.points) {
+            point = this.points[pointIndex];
+            
+            x = matrix[0][0] * point.x + matrix[0][1] * point.y;
+            y = matrix[1][0] * point.x + matrix[1][1] * point.y;
+            
+            points.push(new Point(this.x + x, this.y - y));
+        }
+        
+        ctx.beginPath();
+        ctx.lineWidth = "1";
+        ctx.strokeStyle = "#FFFFFF";
+        
+        ctx.moveTo(points[0].x, points[0].y);
+        
+        for(var i = 1; i < points.length; i++) {
+            point = points[i];
+            ctx.lineTo(point.x, point.y);
+        }
+        
+        if(this.closeShape) {
+            ctx.lineTo(points[0].x, points[0].y);
+        }
+        
+        ctx.stroke();
+    };
+    
+    this.transitioners = {}
+    this.transitionProperty = function(property, goal, step) {
+        var entity = this;
+        
+        this.transitioners[property] = setInterval(function() {
+            if(entity[property] < goal) {
+                entity[property] += step;
+                if(entity[property] > goal) {
+                    entity[property] = goal;
+                    clearInterval(entity.transitioners[property]);
+                }
+            } else if(entity[property] > goal) {
+                entity[property] -= step;
+                if(entity[property] < goal) {
+                    entity[property] = goal;
+                    clearInterval(entity.transitioners[property]);
+                }
+            } else {
+                clearInterval(entity.transitioners[property]);
+            }
+        }, 32);
+    };
+    
+    this.clearTransition = function(property) {
+        clearInterval(this.transitioners[property]);
+    };
 }
 
 function Point(x, y) {
-    point = {};
-    
-    point.x = x;
-    point.y = y;
-    
-    return point;
-}
-
-function transitionProperty(entity, property, goal, step) {
-    stepper = setInterval(function() {
-        if(entity[property] < goal) {
-            entity[property] += step;
-            if(entity[property] > goal) {
-                entity[property] = goal;
-                clearInterval(stepper);
-            }
-        } else if(entity[property] > goal) {
-            entity[property] -= step;
-            if(entity[property] < goal) {
-                entity[property] = goal;
-                clearInterval(stepper);
-            }
-        } else {
-            clearInterval(stepper);
-        }
-    }, 32);
-}
-
-
-function drawEntity(ctx) {
-    matrix = [[Math.cos(this.angle) * this.scale, (Math.sin(this.angle) + this.xShear) * this.scale],
-             [(-Math.sin(this.angle) + this.yShear) * this.scale, Math.cos(this.angle) * this.scale]];
-    
-    points = [];
-    for(pointIndex in this.points) {
-        point = this.points[pointIndex];
-        
-        x = matrix[0][0] * point.x + matrix[0][1] * point.y;
-        y = matrix[1][0] * point.x + matrix[1][1] * point.y;
-        
-        points.push(Point(this.x + x, this.y + y));
-    }
-    
-    ctx.beginPath();
-    ctx.lineWidth = "1";
-    ctx.strokeStyle = "#FFFFFF";
-    
-    ctx.moveTo(points[points.length - 1].x, points[points.length - 1].y);
-    
-    for(pointIndex in points) {
-        point = points[pointIndex];
-        ctx.lineTo(point.x, point.y);
-    }
-    
-    ctx.stroke();
+    this.x = x;
+    this.y = y;
 }
