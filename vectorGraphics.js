edaa914c0fda4569f40828c2cce328a870199440
Zak Stroud <ztstroud@gmail.com>
2021-05-29 15:58:33 -0700

    Switch objects to using classes

--- vectorGraphics.js
+++ vectorGraphics.js
@@ -1,42 +1,53 @@
-var RenderEngine = {
-    canvas: document.getElementById("canvas"),
-    ctx   : this.canvas.getContext("2d")
-};
-
-RenderEngine.render = function(renderable, x, y, angle, scale=1, xShear=0, yShear=0, color="#FFFFFF") {
-    var matrix = [[Math.cos(angle) * scale, (-Math.sin(angle) + xShear) * scale],
-                 [(Math.sin(angle) + yShear) * scale, Math.cos(angle) * scale]];
-    
-    var points = [];
-    for(var pointIndex in renderable.points) {
-        var point = renderable.points[pointIndex];
-        
-        xOffset = matrix[0][0] * point.x + matrix[0][1] * point.y;
-        yOffset = matrix[1][0] * point.x + matrix[1][1] * point.y;
-        
-        points.push(new Point(x + xOffset, y - yOffset));
+class RenderEngine {
+    constructor(canvas) {
+        this.canvas = canvas;
+        this.ctx = canvas.getContext("2d");
     }
-    
-    this.ctx.beginPath();
-    this.ctx.lineWidth = "1";
-    this.ctx.strokeStyle = color;
-    
-    this.ctx.moveTo(points[0].x, points[0].y);
-    for(var pointIndex = 1; pointIndex < points.length; pointIndex++)
-        this.ctx.lineTo(points[pointIndex].x, points[pointIndex].y);
-    
-    if(renderable.closeShape)
-        this.ctx.lineTo(points[0].x, points[0].y);
-    
-    this.ctx.stroke();
+
+    clear() {
+        this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+    }
+
+    render(renderable, x, y, angle, {
+        scale = 1,
+        color = "#FFFFFF",
+        lineWidth = "1px"
+    } = {}) {
+        this.ctx.resetTransform();
+
+        this.ctx.translate(x, y);
+        this.ctx.rotate(angle);
+
+        this.ctx.lineWidth = lineWidth;
+        this.ctx.strokeStyle = color;
+
+        this.ctx.beginPath();
+        this.ctx.moveTo(renderable.points[0].x * scale, renderable.points[0].y * scale);
+
+        for(let index = 1; index < renderable.points.length; index++)
+            this.ctx.lineTo(renderable.points[index].x * scale, renderable.points[index].y * scale);
+
+        if(renderable.closeShape)
+            this.ctx.closePath();
+
+        this.ctx.stroke();
+
+        this.ctx.resetTransform();
+    }
 }
 
-function Renderable(points, closeShape=true) {
-    this.points     = points;
-    this.closeShape = closeShape;
+
+class Renderable {
+    constructor(points, closeShape = true) {
+        this.points = points;
+        this.closeShape = closeShape;
+    }
 }
 
-function Point(x, y) {
-    this.x = x;
-    this.y = y;
+
+class Point {
+    constructor (x, y) {
+        this.x = x;
+        this.y = y;
+    }
 }
